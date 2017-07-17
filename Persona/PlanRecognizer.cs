6bd5cebf00e2be917bced6ca4aeb89d0072c29c2
Rogelio E. Cardona-Rivera <recardon@ncsu.edu>
2017-07-17 15:15:44 -0600

    Committing stubs of the observation compilation script and PlanRecognizer code.

---
+++ Persona/PlanRecognizer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Persona
+{
+    public class PlanRecognizer
+    {
+		static int Main(string[] args)
+		{
+
+
+			return 0;
+		}
+
+        public PlanRecognizer()
+        {
+
+        }
+
+        private void CompileObservations()
+        {
+            // Start the observation compilation bash file.
+            ProcessStartInfo obsCompilerBatchScript;
+
+        }
+
+
+    }
+}
---
+++ Persona/obscompile.sh
@@ -0,0 +1,2 @@
+cd /cygdrive/$1/Benchmarks/$2/
+/cygdrive/$1/ObservationCompiler/pr2plan.exe -d /cygdrive/$1/Benchmarks/$2/domain.pddl -i /cygdrive/$1/Benchmarks/$2/prob$3.pddl -o /cygdrive/$1/Benchmarks/$2/obs.dat -v
