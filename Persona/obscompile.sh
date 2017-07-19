87f53f9c991556d7729f8a080a9d7f748ae1a48c
Rogelio E. Cardona-Rivera <recardon@ncsu.edu>
2017-07-19 13:12:55 -0600

    Getting rid of the observation compilation script.

--- Persona/obscompile.sh
+++
@@ -1,2 +0,0 @@
-cd /cygdrive/$1/Benchmarks/$2/
-/cygdrive/$1/ObservationCompiler/pr2plan.exe -d /cygdrive/$1/Benchmarks/$2/domain.pddl -i /cygdrive/$1/Benchmarks/$2/prob$3.pddl -o /cygdrive/$1/Benchmarks/$2/obs.dat -v
