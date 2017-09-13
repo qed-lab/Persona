2d0ab4fdb2c6571dc568c88221d503051afcb6e8
Rogelio E. Cardona-Rivera <recardon@ncsu.edu>
2017-09-12 22:34:09 -0600

    Adding Combinatorics package from https://github.com/Zjadacz/Combinatorics

---
+++ Persona/Combinatorics/Partitions/SetPartitionGenerator.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GG.Combinatorics.Partitions
+{
+    public class SetPartitionGenerator
+    {
+        /// <summary>
+        /// Simillary to integer partition set partition is used to create different division (subsets) of given set. 
+        /// For example when we try to create partitions for set {a,b,c,d}, we will end up with:
+        /// {a}, {b,c,d}
+        /// {b}, {a,c,d}
+        /// {c}, {a,b,d}
+        /// {d}, {a,b,c}
+        /// {a,b}, {c,d}
+        /// {a,c}, {b,d}
+        /// {a,d}, {b,c}
+        /// {a}, {b}, {c,d}
+        /// {a}, {c}, {b,d}
+        /// {a}, {d}, {b,c}
+        /// {b}, {c}, {a,d}
+        /// {b}, {d}, {a,c}
+        /// {c}, {d}, {a,b}
+        /// {a}, {b}, {c}, {d}
+        /// </summary>
+        /// <typeparam name="T">Type of sets' element</typeparam>
+        /// <param name="collectionOfSets">Collection of sets to partition</param>
+        /// <returns>True if partition was successful, false otherwise.</returns>
+        public bool Next<T>(IList<IList<T>> collectionOfSets) where T: IComparable<T>
+        {
+            if (collectionOfSets.Count > 1)
+            {
+                for (int i = 0; i < collectionOfSets.Count; i++)
+                {
+                    collectionOfSets[i] = collectionOfSets[i].OrderByDescending(k => k).ToList();
+                }
+            }
+
+            var intPartition = CreateIntPartition(collectionOfSets);
+            var flatCollection = collectionOfSets.SelectMany(s => s).ToArray();
+
+            bool wasChange = false;
+
+            if (collectionOfSets.Count > 1)
+            {
+                wasChange = PermutationGenerator.Next(flatCollection);
+            }
+
+            if (!wasChange)
+            {
+                flatCollection = flatCollection.OrderBy(k => k).ToArray();
+                wasChange = new IntegerPartitionGenerator().Next(intPartition);
+            }
+
+            if (wasChange)
+            {
+                var counter = 0;
+                collectionOfSets.Clear();
+                foreach(var count in intPartition)
+                {
+                    collectionOfSets.Add(flatCollection.Skip(counter).Take(count).OrderBy(k => k).ToList());
+                    counter += count;
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public List<int> CreateIntPartition<T>(IList<IList<T>> collectionOfSets)
+        {
+            return collectionOfSets.Select(l => l.Count).ToList();
+        }
+    }
+}
---
+++ Persona/Combinatorics/PermutationGenerator.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace GG.Combinatorics
+{
+    public static class PermutationGenerator
+    {
+        /// <summary>
+        /// Generic implementation of Knuth's permutation algorithm
+        /// 1. Find the largest index j such that a[j] &lt; a[j + 1]. If no such index exists, the permutation is the last permutation.
+        /// 2. Find the largest index l such that a[j] &lt; a[l]. Since j + 1 is such an index, l is well defined and satisfies j &lt; l.
+        /// 3. Swap a[j] with a[l].
+        /// 4. Reverse the sequence from a[j + 1] up to and including the final element a[n].
+        /// </summary>
+        /// <typeparam name="T">Generic type of list elements</typeparam>
+        /// <param name="input">IList collection of element to permutate</param>
+        /// <param name="comparer">Custom T comparer</param>
+        /// <returns>True if permutation was done, false otherwise</returns>
+        public static bool Next<T>(IList<T> input, IComparer<T> comparer = null) where T: IComparable<T>
+        {
+            var largestIndex = -1;
+            for (var i = input.Count - 2; i >= 0; i--)
+            {
+                if ((comparer != null && comparer.Compare(input[i], input[i + 1]) > 0) 
+                    || input[i].CompareTo(input[i + 1]) < 0)
+                {
+                    largestIndex = i;
+                    break;
+                }
+            }
+
+            if (largestIndex < 0)
+            {
+                return false;
+            }
+
+            var largestIndex2 = -1;
+            for (var i = input.Count - 1; i >= 0; i--)
+            {
+                if ((comparer != null && comparer.Compare(input[largestIndex], input[i]) > 0)
+                    || input[largestIndex].CompareTo(input[i]) < 0)
+                {
+                    largestIndex2 = i;
+                    break;
+                }
+            }
+
+            var tmp = input[largestIndex];
+            input[largestIndex] = input[largestIndex2];
+            input[largestIndex2] = tmp;
+
+            for (int i = largestIndex + 1, j = input.Count - 1; i < j; i++, j--)
+            {
+                tmp = input[i];
+                input[i] = input[j];
+                input[j] = tmp;
+            }
+
+            return true; 
+        }
+
+        /// <summary>
+        /// Generic implementation of Knuth's permutation algorithm
+        /// 1. Find the largest index j such that a[j] &lt; a[j + 1]. If no such index exists, the permutation is the last permutation.
+        /// 2. Find the largest index l such that a[j] &lt; a[l]. Since j + 1 is such an index, l is well defined and satisfies j &lt; l.
+        /// 3. Swap a[j] with a[l].
+        /// 4. Reverse the sequence from a[j + 1] up to and including the final element a[n].
+        /// </summary>
+        /// <param name="text">Input text to permutate</param>
+        /// <param name="comparer">Comparer to use</param>
+        /// <returns>True if permutation was done, false otherwise</returns>
+        public static bool Next(ref string text, IComparer<char> comparer = null)
+        {
+            var listOfCharacters = text.ToCharArray();
+            var result = Next(listOfCharacters, comparer);
+            if (result)
+            {
+                text = new string(listOfCharacters);
+            }
+
+            return result;
+        }
+
+        public static long CountAll<T>(IList<T> input, IComparer<T> comparer = null) where T: IComparable<T>
+        {
+            var ordered = comparer != null 
+                ? input.OrderBy(k => k, comparer).ToList() 
+                : input.OrderBy(k => k).ToList();
+
+            int distinctElementsCount = 1;
+            for (int i = 1; i < ordered.Count; i++)
+            {
+                if(ordered[i - 1].CompareTo(ordered[i]) != 0)
+                {
+                    distinctElementsCount++;
+                }
+            }
+
+            return MathHelper.Factorial(distinctElementsCount);
+        }
+    }
+}
