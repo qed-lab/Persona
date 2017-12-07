7cbba540300a233be78af752df04b555905f0f63
Rogelio E. Cardona-Rivera <recardon@ncsu.edu>
2017-12-06 17:51:39 -0700

    Adding tests.

--- Persona/PersonaEvaluation.cs
+++ Persona/PersonaEvaluation.cs
@@ -24,11 +24,19 @@ namespace Persona
             // RunCognitive(IndexterSalienceThreshold.STRICT);
             // Utilities.CorrectDetectedObjectsInGameProblemFiles();
             // PlanFailTest();
+
+            // RunBaseline();
             // RunWindowed();
-            RunBaseline();
+            // RunBaselineWithConservativeDomainExpansion();
+
+
+            // Test
+            TestGoalCombinations();
+
 
             return 0;
         }
+
 
         /// <summary>
         /// Runs the baseline version of the plan recognition pipeline.
@@ -296,146 +304,147 @@ namespace Persona
         /// <summary>
         /// Runs the windowed version of the pipeline with domain expansion.
         /// </summary>
-   //     private static void RunWindowedWithDomainExpansion()
-   //     {
-			//// Record the kind of the system that is running here.
-			//string config = "windowed_with_DE";
+        private static void RunBaselineWithConservativeDomainExpansion()
+        {
+			// Record the kind of the system that is running here.
+            string config = "baseline_with_conservative_DE";
 
-   //         // Load the baseline domain and problem
-   //         Tuple<Domain, Problem> baseline = Utilities.GetBaselineArthurDomainAndProblem();
+            // Load the baseline domain and problem
+            Tuple<Domain, Problem> baseline = Utilities.GetBaselineArthurDomainAndProblem();
 
-			//// Load each folder.
-			//string dataPath = Parser.GetTopDirectory() + @"data/";
-			//string[] dataFolders = Directory.GetDirectories(dataPath);
+			// Load each folder.
+			string dataPath = Parser.GetTopDirectory() + @"data/";
+			string[] dataFolders = Directory.GetDirectories(dataPath);
 
-			//// For each player
-			//foreach (string dataFolder in dataFolders)
-			//{
-			//	// Get the player's ID.
-			//	string[] dataPathString = dataFolder.Split(new char[] { '/' });
-			//	int playerId = Convert.ToInt32(dataPathString[dataPathString.Length - 1]);
+			// For each player
+			foreach (string dataFolder in dataFolders)
+			{
+				// Get the player's ID.
+				string[] dataPathString = dataFolder.Split(new char[] { '/' });
+				int playerId = Convert.ToInt32(dataPathString[dataPathString.Length - 1]);
 
-			//	// Create an output folder.
-			//	string outputFolder = dataFolder + @"/output_" + config + @"/";
-			//	System.IO.Directory.CreateDirectory(outputFolder);
+				// Create an output folder.
+				string outputFolder = dataFolder + @"/output_" + config + @"/";
+				System.IO.Directory.CreateDirectory(outputFolder);
 
-			//	// Store and change the current working directory.
-			//	string oldWD = Directory.GetCurrentDirectory();
-			//	System.IO.Directory.SetCurrentDirectory(outputFolder);
+				// Store and change the current working directory.
+				string oldWD = Directory.GetCurrentDirectory();
+				System.IO.Directory.SetCurrentDirectory(outputFolder);
 
-			//	// Load the player's chronology
-			//	string observationsPath = dataFolder + @"/chronology.pddl";
-   //             Plan fullChronology = Parser.GetPlan(observationsPath, baseline.Item1, baseline.Item2);
-			//	Plan playerChronology = Utilities.RemoveUselessActions(fullChronology);
+                // Load the player's chronology
+                string observationsPath = dataFolder + @"/chronology.pddl";
+                Plan fullChronology = Parser.GetPlan(observationsPath, baseline.Item1, baseline.Item2);
+                Plan revisedChronology = Utilities.RemoveUselessActions(fullChronology);
+                Plan playerOnlyChronology = Utilities.RemoveNonPlayerActions(fullChronology);
 
-			//	// Create the data log.
-			//	string logPath = Directory.GetCurrentDirectory() + @"/data.csv";
-			//	StreamWriter writer = new StreamWriter(logPath, false);
-			//	writer.WriteLine(DataLogEntry.CSVheader());
+				// Create the data log.
+				string logPath = Directory.GetCurrentDirectory() + @"/data.csv";
+				StreamWriter writer = new StreamWriter(logPath, false);
+				writer.WriteLine(DataLogEntry.CSVheader());
 
-			//	// Iterate the player chronology of observations.
-			//	for (int obsId = 0; obsId < playerChronology.Steps.Count; obsId++)
-			//	{
-			//		// Get the first n actions of the player chronology, where n = obsId.
-			//		Plan prefix = playerChronology.Prefix(obsId) as Plan;
-
-			//		// Get a window of the last m = 7 actions of the above prefix.
-   //                 Plan window = new Plan(baseline.Item1, baseline.Item2, ObservationFilter.Windowed(prefix));
-
-   //                 // Get the last operator and its corresponding id.
-   //                 IOperator last = prefix.Steps.Last();
-   //                 int lastId = last.ID;
-
-   //                 // Every step in the chronology has an id equivalent to its index in the list of steps in the plan.
-   //                 // This condition is therefore activated when the step we're looking at is beyond where the step should be in the chronology.
-   //                 if (chronologyStepId != lastId)
-   //                 {
-   //                     // In this situation, we need to get the step before this last one.
-   //                     List<IOperator> allButLast = prefix.Steps.GetRange(0, prefix.Steps.Count - 1);
-
-   //                     if (allButLast.Count == 0)
-   //                         continue;
-
-   //                     IOperator nextToLast = allButLast.Last();
-   //                     int nextToLastId = nextToLast.ID;
-
-			//			// Then, until we get to the id of the last step, we solve the plan recognition problem using the plan
-   //                     // up to and *excluding* the last step, against the domain and problem files of the chronology step id index.
-   //                     // using the index of the updated domain and problem files.
-			//			while(chronologyStepId != lastId)
-   //                     {
-   //                         Plan prefixMinusLast = new Plan(baselineDomain, baselineProblem, allButLast);
-   //                         Plan windowMinusLast = new Plan(baselineDomain, baselineProblem, ObservationFilter.Windowed(prefixMinusLast));
-
-   //                         string intermediateDomainPath = dataFolder + @"/domain_arthur" + chronologyStepId + @".pddl";
-   //                         string intermediateProblemPath = dataFolder + @"/corrected_problems/problem_arthur" + chronologyStepId + @".pddl";
-
-   //                         Domain intermediateDomain = Parser.GetDomain(intermediateDomainPath, Mediation.Enums.PlanType.StateSpace);
-   //                         Problem intermediateProblem = Parser.GetProblem(intermediateProblemPath);
-
-			//				// Start the data entry.
-   //                         DataLogEntry intermediateLogEntry = new DataLogEntry();
-			//				intermediateLogEntry.PlayerId = playerId;
-			//				intermediateLogEntry.SystemConfiguration = config;
-   //                         intermediateLogEntry.NumberOfObservationsInput = windowMinusLast.Steps.Count;
+                // Get the initial domain and problem files.
+                Tuple<Domain, Problem> microtheory = Utilities.GetIndexedArthurDomainAndProblem(dataFolder, 0);
+                Plan prefix = null;
 
 
-   //                         intermediateLogEntry.NumberOfGoals = intermediateProblem.Goals.Count;
-			//				intermediateLogEntry.NumberOfOperatorsPreCompilation = intermediateDomain.Operators.Count;
-			//				intermediateLogEntry.NumberOfPredicatesPreCompilation = intermediateDomain.Predicates.Count;
-			//				intermediateLogEntry.NumberOfPlayerActionsTaken = obsId;
-			//				intermediateLogEntry.ActualPlan = playerChronology;
+                // Iterate over all player micro-theory domain / problem files.  The system produced a pair of
+                // micro-theory based domain and problem files for every *player* action (including ones that
+                // are deemed useless by the Utilities.RemoveUselessActions(...) above).  So, iterate the domain
+                // and problem files, using as the index a given step of the player only chronology.
+                for (int i = 1; i < playerOnlyChronology.Steps.Count; i++)
+                {
+                    // This is where it gets tricky.
+                    // Up to here, we have:
+                    // revisedChronology \subset playerOnlyChronology \subset fullChronology.
+
+                    // Because of this, it's possible that the steps we're iterating through in the player only chronology
+                    // aren't in the revised chronology at all.  In other words, it may be that:
+                    // > there is no prefix worth considering, or
+                    // > there is no change in the prefix worth considering
+
+                    // Therefore, we have to check 
 
 
-			//				// Assemble an intermediate theory to solve.
-			//				PlanRecognitionProblem intermediateTheory = new PlanRecognitionProblem(intermediateDomain, intermediateProblem, windowMinusLast);
-
-   //                         // Solve the theory
-   //                         intermediateTheory.Solve(intermediateLogEntry);
-
-   //                         chronologyStepId++;
-			//			}
-   //                 }
-
-   //                 // The id of the last operator is the index of the domain and problem that we should load for
-   //                 // the computation of the plan.  Therefore, load the corresponding domain and problem files.
-   //                 string domainPath = dataFolder + @"/domain_arthur" + lastId + @".pddl";
-   //                 string problemPath = dataFolder + @"/corrected_problems/problem_arthur" + lastId + @".pddl";
-
-   //                 // Load the domain and problem for the current observation.
-   //                 Domain domain = Parser.GetDomain(domainPath, Mediation.Enums.PlanType.StateSpace);
-   //                 Problem problem = Parser.GetProblem(problemPath);
-
-			//		// Start the data entry.
-			//		DataLogEntry logEntry = new DataLogEntry();
-			//		logEntry.PlayerId = playerId;
-			//		logEntry.SystemConfiguration = config;
-			//		logEntry.NumberOfObservationsInput = window.Steps.Count;
-
-			//		logEntry.NumberOfGoals = problem.Goals.Count;
-			//		logEntry.NumberOfOperatorsPreCompilation = domain.Operators.Count;
-			//		logEntry.NumberOfPredicatesPreCompilation = domain.Predicates.Count;
-			//		logEntry.NumberOfPlayerActionsTaken = obsId;
-			//		logEntry.ActualPlan = playerChronology;
 
 
-			//		// Assemble a plan recognition theory to solve.
-			//		PlanRecognitionProblem theory = new PlanRecognitionProblem(domain, problem, window);
 
-			//		// Solve the theory.
-			//		theory.Solve(logEntry);
 
-			//		// Add the entry to the log.
-			//		writer.WriteLine(logEntry.ToCSVString());
-			//	}
 
-			//	// Close the log.
-			//	writer.Close();
+                }
 
-			//	// Restore the old working directory.
-			//	Directory.SetCurrentDirectory(oldWD);
-			//}
-        //}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                // Iterate the player chronology of observations.
+                for (int i = 0; i < revisedChronology.Steps.Count; i++)
+                {
+                    // Get the current observation step's ID.
+                    int currentStepId = revisedChronology.Steps.ElementAt(i).ID;
+
+                    // Get the next observation step's ID; there may not be a next one, so check for bounds.
+                    int nextStepId = i + 1 < revisedChronology.Steps.Count ? revisedChronology.Steps.ElementAt(i + 1).ID : currentStepId;
+
+                    // The current and next observation steps are not guaranteed to be continuous (because of the RemoveUselessActions)
+                    // Further, the domain may have expanded in between the current and next steps. So, load all the domains / problems
+                    // between the current and next (exclusive) observation steps and attempt to solve with them.
+
+                    // Iterate the full chronology of observations between the observation steps.
+                    for (int j = currentStepId; j < nextStepId; j++)
+                    {
+                        // Get the domain and problem @ index j.
+                        Tuple<Domain, Problem> expanded = Utilities.GetIndexedArthurDomainAndProblem(dataFolder, j);
+
+                        // If the player has not adopted any goals, skip.
+                        if (expanded.Item2.Goal.Count == 0)
+                            continue;
+
+                        // Otherwise, we can solve this.
+                        else
+                        {
+                            // Get the plan up until this observation.
+                            prefix = revisedChronology.Prefix(i) as Plan;
+
+                            // Construct the plan recognition theory with this prefix and the loaded domain and problem.
+                            PlanRecognitionProblem theory = new PlanRecognitionProblem(expanded.Item1, expanded.Item2, prefix);
+
+                            // Start the data entry.
+                            DataLogEntry logEntry = new DataLogEntry();
+                            logEntry.PlayerId = playerId;
+                            logEntry.SystemConfiguration = config;
+                            logEntry.NumberOfObservationsInput = prefix.Steps.Count;
+                            logEntry.NumberOfGoals = expanded.Item2.Goals.Count;
+                            logEntry.NumberOfOperatorsPreCompilation = expanded.Item1.Operators.Count;
+                            logEntry.NumberOfPredicatesPreCompilation = expanded.Item1.Predicates.Count;
+                            logEntry.NumberOfPlayerActionsTaken = i + 1;
+                            logEntry.ActualPlan = revisedChronology;
+
+                            // Solve the plan recognition theory.
+                            theory.Solve(logEntry);
+
+                            // Add the entry to the log.
+                            writer.WriteLine(logEntry.ToCSVString());
+                        }
+                    }
+                }
+
+                // Close the log.
+                writer.Close();
+
+                // Restore the old working directory.
+                Directory.SetCurrentDirectory(oldWD);
+			}
+        }
 
 
 		/// <summary>
@@ -529,7 +538,7 @@ namespace Persona
 		}
 
 
-        private static void PlanFailTest()
+        private static void TestPlanFail()
         {
             // Load a domain that will fail
             string domain_22 = Parser.GetTopDirectory() + @"data/-5811686/domain_arthur22.pddl";
@@ -542,6 +551,32 @@ namespace Persona
 
         }
 
+
+        private static void TestGoalCombinations()
+        {
+
+            // Load the baseline domain.
+            string domainPath = Parser.GetTopDirectory() + @"benchmarks/baselinedomain.pddl";
+            Domain domain = Parser.GetDomain(domainPath, Mediation.Enums.PlanType.StateSpace);
+
+            // Load the test problem.
+            string testProblemPath = Parser.GetTopDirectory() + @"test/425782449/corrected_problems/problem_arthur160.pddl";
+            Problem problem = Parser.GetProblem(testProblemPath);
+
+            List<List<IPredicate>> disjunction = Utilities.DisjunctifyGoals(problem.Goal);
+            List<List<List<IPredicate>>> goalTriples = Utilities.ExtractGoalTriples(disjunction);
+            List<List<List<IPredicate>>> goalQuadruples = Utilities.ExtractGoalQuadruples(disjunction);
+
+            List<List<List<IPredicate>>> goalCombinations = new List<List<List<IPredicate>>>();
+            goalCombinations.AddRange(goalTriples);
+            goalCombinations.AddRange(goalQuadruples);
+
+            if (disjunction.Count == 5)
+                goalCombinations.Add(disjunction);
+
+            Console.WriteLine(Utilities.GoalCombinationsToPDDL(goalCombinations));
+
+        }
 
 
 
